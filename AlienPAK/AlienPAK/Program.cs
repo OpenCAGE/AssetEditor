d70ee3a0eeb9f7585f83b8bc18e49690c5aaf716
MattFiler <matthew.filer@hotmail.co.uk>
2020-05-17 01:37:15 +0100

    Fix for library linking issue

--- AlienPAK/AlienPAK/Program.cs
+++ AlienPAK/AlienPAK/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,6 +15,14 @@ namespace AlienPAK
         [STAThread]
         static void Main(string[] args)
         {
+            //Need DLLs in directory for image previews to work :(
+            if (!File.Exists("DirectXTexNet.dll")) File.WriteAllBytes("DirectXTexNet.dll", Properties.Resources.DirectXTexNet);
+            Directory.CreateDirectory("x86");
+            if (!File.Exists("x86/DirectXTexNetImpl.dll")) File.WriteAllBytes("x86/DirectXTexNetImpl.dll", Properties.Resources.DirectXTexNetImpl_86);
+            Directory.CreateDirectory("x64");
+            if (!File.Exists("x64/DirectXTexNetImpl.dll")) File.WriteAllBytes("x64/DirectXTexNetImpl.dll", Properties.Resources.DirectXTexNetImpl_64);
+
+            //Launch application
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Explorer(args));
